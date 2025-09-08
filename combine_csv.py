2c94115056b3a1525efac0a99a0e7ac7b7aec39e
arifarman02 <arifarman1820@gmail.com>
2025-09-08 18:26:58 +0100

    Working adding second season data into matchs csv

--- combine_csv.py
+++ combine_csv.py
@@ -22,3 +22,12 @@ matches = pd.concat(all_dfs, ignore_index=True)
 
 # Save the comined dataframe into a csv file
 matches.to_csv('matches.csv', index=False)
+
+# Get the second season data
+second_csv_file_list = os.listdir('./EPL_24-25_season')
+second_csv_file_list.sort()
+
+for csv_file in second_csv_file_list:
+    full_path = os.path('./EPL_24-25_season', csv_file)
+    match = pd.read_csv(full_path, encoding='latin-1')
+    
